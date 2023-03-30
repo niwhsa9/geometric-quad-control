b2b1f06b7446c3df81f308781c9255fed053d786
Ashwin Gupta <ashwingupta2000@gmail.com>
2023-03-30 14:51:54 -0400

    compiles

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Geometric Quadcopter Control
 Quadcopter state estimation and control stack that utilizes the structure of underlying state Lie groups to maximize flight capabilities.
 
+State estimation is achieved via an Extended Kalman Filter on SE(3). Trajectories can be minimally specified in x, y, z, yaw due to the differential flatness of the dynamics.
+
 Dependencies: 
 - Eigen3
 - manif 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <manif/manif.h>
+#include <webots/Robot.hpp>
+#include <webots/Gyro.hpp>
+#include <iostream>
+#include <memory>
+
+using namespace webots;
 
 int main() {
-   auto state = manif::SE3d::Identity(); 
-   std::cout << "test " << state << std::endl;
+  auto robot = std::make_unique<Robot>();
+  auto gyro = robot->getGyro("gyro");
 
+  while (robot->step(32) != -1) {
+    auto v = gyro->getValues();
+    std::cout << "Hello World! " << v[0] << " " << v[1] << " "<< std::endl;
+  }
+
+  return 0;
 }
