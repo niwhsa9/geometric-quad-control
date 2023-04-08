a99e51f84ca83b2ff207f9c6c15d9fed93cc8a03
Ashwin Gupta <ashwingupta2000@gmail.com>
2023-04-08 00:13:50 -0400

    bump

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,15 @@
 #include <memory>
 #include "ekf.h"
 #include "teleop.h"
+#include <random>
 
 using namespace webots;
 
-Eigen::Vector3d get_gps() {
-
-}
-Eigen::Vector3d get_true_pos() {
-
+Eigen::Vector3d add_noise(Eigen::Vector3d vec, std::normal_distribution<double> &nd, std::mt19937 &gen) {
+    vec.x() += nd(gen);
+    vec.y() += nd(gen);
+    vec.z() += nd(gen);
+    return vec;
 }
 
 int main() {
@@ -26,6 +27,10 @@ int main() {
   auto gyro = robot->getGyro("gyro");
   auto accel = robot->getAccelerometer("accelerometer");
   auto gps = robot->getGPS("gps");
+
+  std::random_device rd; 
+  std::mt19937 gen(rd()); 
+  std::normal_distribution<double> nd(0.0, 0.1); 
 
   Teleop teleop(robot.get());
   auto dt = robot->getBasicTimeStep();
@@ -43,25 +48,25 @@ int main() {
   int iter_cnt = 0;
 
   while (robot->step(dt) != -1) {
-    auto v = gyro->getValues();
-    auto a = accel->getValues();
-    auto g = gps->getValues();
-    auto g_v = gps->getSpeedVector();
+    Eigen::Vector3d omega(gyro->getValues());
+    Eigen::Vector3d a(accel->getValues());
+    Eigen::Vector3d gps_pos(gps->getValues());
+    Eigen::Vector3d gps_vel(gps->getSpeedVector());
+
+    Eigen::Vector3d noisy_gps_pos = add_noise(gps_pos, nd, gen);
     
     // robot teleop
     teleop.keyboard_ctrl();
 
     // TODO skip start iterations due to strange contact forces at init in sim
-    if(iter_cnt > 1500 && !isnan(a[0]) && !isnan(g[0])) {
-      ekf.predict(Eigen::Vector3d(v), Eigen::Vector3d(a));
-      ekf.update_gps(Eigen::Vector3d(g), Eigen::Vector3d(g_v));
+    if(iter_cnt > 0 && !isnan(a.x()) && !isnan(gps_pos.x())) {
+      ekf.predict(omega, a);
+      ekf.update_gps(noisy_gps_pos, gps_vel);
       auto state = ekf.get_state();
-      std::cout << "ekf " << state.x() << " " << state.y() <<  " " << state.z()  << " "<< "truth " << g[0] << " " << g[1] <<  " " << g[2] << " "<< std::endl;
+      std::cout << "ekf " << state.x() << " " << state.y() <<  " " << state.z()  << " "<< 
+        "truth " << gps_pos.x() << " " << gps_pos.y() <<  " " << gps_pos.z() << " "<< std::endl;
     }
 
-    //std::cout << "ekf  " << state.X.x() << " " << state.X.y() <<  " " << state.X.z()  << " "<< std::endl
-    //std::cout << "rot  " << state.X.quat().x() << " "<< state.X.quat().y() << " "<< state.X.quat().z() << " "  << state.X.quat().w() << std::endl;
-    //std::cout << "truth  " << g[0] << " " << g[1] <<  " " << g[2] << " "<< std::endl;
     iter_cnt++; 
     
   }
